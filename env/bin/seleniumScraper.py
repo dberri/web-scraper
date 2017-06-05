83d0d724e0e3a311a102bdd7e39795b066670afb
David Berri <dwbwill@gmail.com>
2017-06-05 17:55:40 -0300

    tested with two urls

---
+++ env/bin/seleniumScraper.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+
+def start(url):
+    for u in url:
+        driver.get(u)
+        spider = Spider()
+
+
+class Picture(object):
+
+    def __init__(self, name, url):
+        self.name = name
+        self.url = url
+
+    def download_pic(self):
+        pass
+
+
+class Spider(object):
+
+    def __init__(self):
+        pass
+
+    def create_folder():
+        pass
+
+    def get_img_links():
+        pass
+
+
+
+if __name__ == '__main__':
+    driver = webdriver.Chrome()
+    print('yeah')
+    start(['',
+    ''])
+    driver.close()
